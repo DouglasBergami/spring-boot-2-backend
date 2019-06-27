e4a9d5e49236b977b960117d07af5f35817081ba
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-06-26 21:17:45 -0300

    Cliente, TipoCliente, telefones e enderecos

--- src/main/java/com/douglas/cursomc/CursomcApplication.java
+++ src/main/java/com/douglas/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.douglas.cursomc.domain.Categoria;
 import com.douglas.cursomc.domain.Cidade;
+import com.douglas.cursomc.domain.Cliente;
+import com.douglas.cursomc.domain.Endereco;
 import com.douglas.cursomc.domain.Estado;
 import com.douglas.cursomc.domain.Produto;
+import com.douglas.cursomc.domain.enums.TipoCliente;
 import com.douglas.cursomc.repositories.CategoriaRepositoy;
 import com.douglas.cursomc.repositories.CidadeRepository;
+import com.douglas.cursomc.repositories.ClienteRepository;
+import com.douglas.cursomc.repositories.EnderecoRepository;
 import com.douglas.cursomc.repositories.EstadoRepository;
 import com.douglas.cursomc.repositories.ProdutoRepository;
 
@@ -27,42 +32,59 @@ public class CursomcApplication implements CommandLineRunner {
 	private CidadeRepository cidadeRepository;
 	@Autowired
 	private EstadoRepository estadoRepository;
-	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	@Autowired
+	private EnderecoRepository enderecoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		
+
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		
+
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
-		
+
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
-		
+
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
-		
-		Estado estado1 = new Estado (null, "Minas Gerais");
-		Estado estado2 = new Estado (null, "São Paulo");
-		
-		Cidade c1 = new Cidade (null, "Uberlândia", estado1);
-		Cidade c2 = new Cidade (null, "São Paulo", estado2);
-		Cidade c3 = new Cidade (null, "Campinas", estado2);
-		
+
+		Estado estado1 = new Estado(null, "Minas Gerais");
+		Estado estado2 = new Estado(null, "São Paulo");
+
+		Cidade c1 = new Cidade(null, "Uberlândia", estado1);
+		Cidade c2 = new Cidade(null, "São Paulo", estado2);
+		Cidade c3 = new Cidade(null, "Campinas", estado2);
+
 		estado1.getCidades().addAll(Arrays.asList(c1));
 		estado2.getCidades().addAll(Arrays.asList(c2, c3));
-				
+
 		estadoRepository.saveAll(Arrays.asList(estado1, estado2));
 		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
+		Cliente cli1 = new Cliente(null, "Maria Silva", "maria@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+
+		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
+
+		Endereco e1 = new Endereco(null, "Rual Flores", "300", "Apto 203", "Jardim", "38220834", cli1, c1);
+		Endereco e2 = new Endereco(null, "Avenida Matos", "105", "Sala 800", "Centro", "38777012", cli1, c2);
+
+		cli1.getEnderecos().addAll(Arrays.asList(e1, e2));
+
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		enderecoRepository.saveAll(Arrays.asList(e1, e2));
+
 	}
 }
---
+++ src/main/java/com/douglas/cursomc/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.cursomc.domain.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
+
+}
