8a0b7239b15355628b4829f401649f3e0a71dcd6
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-06-27 00:58:04 -0300

    Pedido, EstadoPagamento e Pagamento

---
+++ src/main/java/com/douglas/cursomc/repositories/PagamentoRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.cursomc.domain.Pagamento;
+
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer>{
+
+}
---
+++ src/main/java/com/douglas/cursomc/repositories/PedidoRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.cursomc.domain.Pedido;
+
+public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+
+}
