e8e780a681eb3d129475c72d34bae8658a4ee4bf
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-06-28 00:51:29 -0300

    ItemPedido e ItemPedidoPK

---
+++ src/main/java/com/douglas/cursomc/repositories/ItemPedidoRepository.java
@@ -0,0 +1,9 @@
+package com.douglas.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.douglas.cursomc.domain.ItemPedido;
+
+public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Integer>{
+
+}
