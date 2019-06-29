87d3e2f9673377229ec7da510b16c76c9dc52442
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-06-29 13:15:15 -0300

    Listando todas categorias com DTO

--- src/main/java/com/douglas/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/douglas/cursomc/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.douglas.cursomc.resources;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.douglas.cursomc.domain.Categoria;
+import com.douglas.cursomc.dto.CategoriaDTO;
 import com.douglas.cursomc.services.CategoriaService;
 
 @RestController
@@ -29,29 +33,48 @@ public class CategoriaResource {
 		return ResponseEntity.ok().body(obj);
 
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST)
 	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
 
 		obj = categoriaService.insert(obj);
 
-		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
-				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 
 		return ResponseEntity.created(uri).build();
 
 	}
-	
+
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
-	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id) {
 		obj.setId(id);
 		obj = categoriaService.update(obj);
 		return ResponseEntity.noContent().build();
 	}
+
 	@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
-	public ResponseEntity<Void> delete(@PathVariable Integer id){
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		categoriaService.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> lista = categoriaService.findAll();
+
+		/*
+		 * pode se fazer assim List<CategoriaDTO> listadto = new
+		 * ArrayList<CategoriaDTO>(); 
+		 * for (Categoria x : lista) { 
+		 * listadto.add(new CategoriaDTO(x)); 
+		 * }
+		 */
+
+		// ou assim
+
+		List<CategoriaDTO> listadto = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+
+		return ResponseEntity.ok().body(listadto);
 	}
 
 }
--- src/main/java/com/douglas/cursomc/services/CategoriaService.java
+++ src/main/java/com/douglas/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.douglas.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -38,12 +39,16 @@ public class CategoriaService {
 
 	public void delete(Integer id) {
 		find(id);
-		try{
+		try {
 			repository.deleteById(id);
-		}catch(DataIntegrityViolationException e) {
+		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
 		}
-		
+
+	}
+
+	public List<Categoria> findAll() {
+		return repository.findAll();
 	}
 
 }
