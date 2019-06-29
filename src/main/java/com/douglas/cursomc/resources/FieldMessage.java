be738d7cb4bad3b1f2a88d9879371b9565cbc39f
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-06-29 15:43:58 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/douglas/cursomc/resources/FieldMessage.java
@@ -0,0 +1,36 @@
+package com.douglas.cursomc.resources;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private String field;
+	private String message;
+
+	public FieldMessage() {
+	}
+
+	public FieldMessage(String field, String message) {
+		super();
+		this.field = field;
+		this.message = message;
+	}
+
+	public String getField() {
+		return field;
+	}
+
+	public void setField(String fileName) {
+		this.field = fileName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
