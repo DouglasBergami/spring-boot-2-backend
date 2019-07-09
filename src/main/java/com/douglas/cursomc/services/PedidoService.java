6b7fdef1823928720cae33b4c56ec49296f64f78
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-07-09 11:18:14 -0300

    MockEmailService com Logger. Padroes Strategy e Template Method

--- src/main/java/com/douglas/cursomc/services/PedidoService.java
+++ src/main/java/com/douglas/cursomc/services/PedidoService.java
@@ -31,6 +31,8 @@ public class PedidoService {
 	private ProdutoService produtoService;
 	@Autowired
 	private ClienteService clienteService;
+	@Autowired
+	private EmailService emailService;
 
 	public Pedido find(Integer id) {
 
@@ -64,7 +66,7 @@ public class PedidoService {
 		}
 		
 		itemPedidoRepository.saveAll(obj.getItens());
-		System.out.println(obj);
+		emailService.sendOrderConfirmationEmail(obj);
 		return obj;
 	}
 
