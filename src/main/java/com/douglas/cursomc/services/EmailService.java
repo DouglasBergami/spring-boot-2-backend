8868a1de0928235bcd170f5b5f0448c85a668509
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-07-09 17:17:13 -0300

    Email HTML

--- src/main/java/com/douglas/cursomc/services/EmailService.java
+++ src/main/java/com/douglas/cursomc/services/EmailService.java
@@ -1,13 +1,18 @@
 package com.douglas.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.douglas.cursomc.domain.Pedido;
 
 public interface EmailService {
-	
-	void sendOrderConfirmationEmail (Pedido obj);
-	
-	void sendEmail (SimpleMailMessage msg);
 
+	void sendOrderConfirmationEmail(Pedido obj);
+
+	void sendEmail(SimpleMailMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+
+	void sendHtmlEmail(MimeMessage msg);
 }
