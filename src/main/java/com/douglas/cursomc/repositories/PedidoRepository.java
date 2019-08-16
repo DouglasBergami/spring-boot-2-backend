1bfd0060e56a26f8ec7156173e842600c49ae839
douglas.bergami <douglas.t.k.d@hotmail.com>
2019-08-15 22:47:48 -0300

    Restrição de conteúdo: cliente só recupera seus pedidos

--- src/main/java/com/douglas/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/douglas/cursomc/repositories/PedidoRepository.java
@@ -1,9 +1,15 @@
 package com.douglas.cursomc.repositories;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.douglas.cursomc.domain.Cliente;
 import com.douglas.cursomc.domain.Pedido;
 
-public interface PedidoRepository extends JpaRepository<Pedido, Integer>{
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 
+	@Transactional(readOnly = true)
+	Page<Pedido> findByCliente(Cliente cliente, Pageable pageRequest);
 }
--- src/main/java/com/douglas/cursomc/services/PedidoService.java
+++ src/main/java/com/douglas/cursomc/services/PedidoService.java
@@ -4,9 +4,14 @@ import java.util.Date;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.douglas.cursomc.domain.Categoria;
+import com.douglas.cursomc.domain.Cliente;
 import com.douglas.cursomc.domain.ItemPedido;
 import com.douglas.cursomc.domain.PagamentoComBoleto;
 import com.douglas.cursomc.domain.Pedido;
@@ -14,6 +19,8 @@ import com.douglas.cursomc.domain.enums.EstadoPagamento;
 import com.douglas.cursomc.repositories.ItemPedidoRepository;
 import com.douglas.cursomc.repositories.PagamentoRepository;
 import com.douglas.cursomc.repositories.PedidoRepository;
+import com.douglas.cursomc.security.UserSS;
+import com.douglas.cursomc.services.exception.AuthorizationException;
 import com.douglas.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -68,6 +75,19 @@ public class PedidoService {
 		itemPedidoRepository.saveAll(obj.getItens());
 		emailService.sendOrderConfirmationHtmlEmail(obj);
 		return obj;
+	}
+
+	public Page<Pedido> findPage(Integer page, Integer linesPerPage, String orderBy, String direction) {
+		
+		UserSS user = UserService.authenticated();
+		
+		if (user==null) {
+			throw new AuthorizationException("Acesso Negado");
+		}
+		
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Cliente cliente = clienteService.find(user.getId());
+		return pedidoRepository.findByCliente(cliente, pageRequest);
 	}
 
 }
